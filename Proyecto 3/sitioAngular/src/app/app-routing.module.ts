49e80b4233215432a40bda47dfad2f404ab169bb
gaveinti <gaveinti>
2022-08-05 12:33:01 -0500

    Diseño de inicio y enrutar

--- Proyecto 3/sitioAngular/src/app/app-routing.module.ts
+++ Proyecto 3/sitioAngular/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { InfoComponent } from './info/info.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: "inicio", component: InicioComponent },
+  { path: "info", component: InfoComponent },
+  { path: "**", redirectTo: "inicio" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Proyecto 3/sitioAngular/src/app/app.module.ts
+++ Proyecto 3/sitioAngular/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InicioComponent } from './inicio/inicio.component';
+import { MainComponent } from './main/main.component';
+import { NewsComponent } from './news/news.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    MainComponent,
+    NewsComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
